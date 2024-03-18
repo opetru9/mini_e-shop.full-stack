f89d0e71ba3d0d33b6ca0ba55b5b0051f598888c
opetru9 <opetrik9@mail.ru>
2024-03-18 12:26:35 +0200

    added templates

--- src/app.mjs
+++ src/app.mjs
@@ -1,12 +1,12 @@
 import { getProductById, addClient, placeOrder, 
             getClientById, getClientByEmailOrPhone } from "./modules/e_shop.mjs";
 
-// let product  = await getProductById(1)
+let product  = await getProductById(1)
 // let client   = await getClientById(2)
 // let order    = await placeOrder(client, product, 3)
 // let client   = await addClient('Peter Parcker', 'peter@mail.ro', '+123456789' )
 
-let client = await getClientByEmailOrPhone("+123456789")
+// let client = await getClientByEmailOrPhone("+123456789")
 
-console.log(client)
+console.log(product)
     
--- src/server.mjs
+++ src/server.mjs
@@ -1,20 +1,49 @@
 import {createServer} from 'node:http'
+import { readFile } from 'node:fs/promises';
+import {
+  getProductById,
+  addClient,
+  placeOrder,
+  getClientById,
+  getClientByEmailOrPhone,
+} from "./modules/e_shop.mjs";
 
-const server = createServer((req, res) => {
+ const server = createServer(async (req, res) => {
     let html;
     switch (req.url){
         case '/': 
-                html= '<h1>HOME PAGE<h1/>';
+                let product = await getProductById(1)
+                html = (await readFile('./templates/home.html')).toString();
+                html = html.replace(
+                  "{%product%}",
+                  `<h1>${product.name}</h1>
+                        <img src='./img/${product.img}'>
+                        <br>
+                        <h3> Price: ${product.standartPrice.amount} 
+                        ${product.standartPrice.currency}</h3>
+                        <h3> Discount Price: ${product.discountPrice.amount} 
+                        ${product.discountPrice.currency}</h3>  
+                        <a href= '/order'> PAY </a>  
+                `);
         break;
         case '/order': 
-                html= '<h1>ORDER<h1/>';
+                html = (await readFile("./templates/order.html")).toString();
+        break;
+        case '/order-confirm': 
+                html = 'ORDER CONFIRMED';
         break;
         case '/contacts': 
                 html= '<h1>CONTACTS INFO<h1/>';
         break;
         default:
-            html = '404: page not found'
-            res.writeHead(404)
+            if (req.url.startsWith('/img')) {
+                let parts = req.url.split('/');
+                let img = parts.pop();
+                html = await readFile(`./img/${img}`);
+            } else {
+                html = '404: Page not found';
+                res.writeHead(404);
+            }
     }
 
     res.end(html)
